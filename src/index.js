25d57623f28c1bd6fe7bf306fa465f8130f448ef
Comingsoon <comingsoon000@ya.ru>
2021-03-09 21:51:23 +0300

    init: start Typical Arrays Problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+const arraySort = array => array.sort((a, b) => a - b);
+const itemsSum = array => array.reduce((acc, item) => acc + item, 0);
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array) {
+    return 0;
+  }
+  return ((array.length > 0) ? arraySort(array)[0] : 0);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array) {
+    return 0;
+  }
+  return ((array.length > 0) ? arraySort(array)[array.length - 1] : 0);;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array) {
+    return 0;
+  }
+  return ((array.length > 0) ? itemsSum(array) / array.length : 0);
 }
